c0f765b963fede38cce7ea3c4b284d71dad25bae
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2024-01-29 16:42:48 +0600

    inital

---
+++ app/src/main/java/com/khalekuzzaman/just/cse/dsavisualier2024/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.khalekuzzaman.just.cse.dsavisualier2024
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.khalekuzzaman.just.cse.dsavisualier2024.ui.theme.DSAVisualier2024Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DSAVisualier2024Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DSAVisualier2024Theme {
+        Greeting("Android")
+    }
+}
